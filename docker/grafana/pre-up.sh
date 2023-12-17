7da6ea3e3966cbe8e99f2f0dcc7674b0f88d238f
Your Name <you@example.com>
2023-12-17 10:35:24 +0800

    update

--- docker/grafana/pre-up.sh
+++ docker/grafana/pre-up.sh
@@ -1,1 +1,2 @@
-docker run --rm --entrypoint "cat"  grafana/grafana-enterprise:10.2.2-boringcrypto "/etc/grafana/grafana.ini" > grafana.example.ini
+mkdir data
+docker run --rm --entrypoint "cat"  grafana/grafana-enterprise:10.2.2-boringcrypto "/etc/grafana/grafana.ini" > data/grafana.ini
